32960bfdebc616732de9dd673d04cd9e64a04a33
Urwashi Makheja <162986350+Urwashi123@users.noreply.github.com>
2024-07-16 00:07:52 +0530

    Add files via upload

--- Contact.jsx
+++ Contact.jsx
@@ -1,54 +1,78 @@
-import React from 'react'
-import "./Contact.css"
-import Swal from 'sweetalert2'
+import React, { useState } from 'react';
+import "./Contact.css";
+
 
 const Contact = () => {
-  
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
+  const [formData, setFormData] = useState({
+      name: '',
+      email: '',
+      message: '',
+    })
+  const [issubmmited, setIssubmmited] = useState(false);
 
-    formData.append("access_key", "b08483fe-6c72-4e6c-b951-ee2c99b05ab6");
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIssubmmited(true);
+    hideForm();
+  }
 
-    const object = Object.fromEntries(formData);
-    const json = JSON.stringify(object);
 
-    const res = await fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      },
-      body: json
-    }).then((res) => res.json());
-
-    if (res.success) {
-      Swal.fire({
-        title: "Sucess!",
-        text: "Message sent sucessfully!",
-        icon: "success"
-      });
-    }
-  };
+const hideForm = () => {
+  setFormData({
+    name: '',
+    email: '',
+    message: '',
+  })
+}
   return (
     <section className='Contact'>
-        <form onSubmit={onSubmit}>
-            <h2>Contact form </h2>
-            <div className="input-box">
-                <label>Full Name</label>
-                <input type="text" className="field" name='name' placeholder='Enter your name'required/>
-                </div>
-                <div className="input-box">
-                <label>Email Address</label>
-                <input type="email" className="field"name='email' placeholder='Enter your email'required/>
-                </div>
-                <div className="input-box">
-                <label>Your Message</label>
-               <textarea name="message" className="field mess" placeholder='Enter your message'required></textarea>
-                </div>
-                <button type="submit">Send Message</button>
+      {issubmmited ? (
+        <div className='submit'>Form submitted successfully!</div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <h2>Contact form </h2>
+          <div className="input-box">
+            <label>Full Name</label>
+            <input
+              type="text"
+              className="field"
+              onChange={handleChange}
+              value={formData.name}
+              name='name'
+              placeholder='Enter your name'
+              required />
+          </div>
+          <div className="input-box">
+            <label>Email Address</label>
+            <input type="email"
+              className="field"
+              name='email'
+              onChange={handleChange}
+              value={formData.email}
+              placeholder='Enter your email'
+              required />
+          </div>
+          <div className="input-box">
+            <label>Your Message</label>
+            <textarea
+              name="message"
+              className="field mess"
+              placeholder='Enter your message'
+              onChange={handleChange}
+              value={formData.message}
+              required>
+            </textarea>
+          </div>
+          <button type="submit">Send Message</button>
         </form>
-
+      )}
     </section>
   )
 }
